82b133d026e853a4b3092f0441da6beb28cf8a64
Michael Pack <packmichael@hotmail.com>
2023-09-18 19:45:51 -0230

    imports added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import Nav from "./components/Nav";
+import Friend from "./components/Friend";
+import Message from "./components/Message";
+import Photo from "./components/Photo";
+import Profile from "./components/Profile";
+import Video from "./components/Video";
 
 function App() {
   return (
