5fbc5eb6f3c50de98166afff2070f3d9e811c0fd
Michael Pack <packmichael@hotmail.com>
2023-11-01 21:31:41 -0230

    initial clickable image component

--- src/components/Photoclick.tsx
+++ src/components/Photoclick.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import { Button, TextField } from "@mui/material";
 
-const Photoclick = () => {
-  return <div>Photoclick</div>;
+const ImageWithDialog = ({ imageSrc, initialComments = [] }) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [comments, setComments] = useState(initialComments);
+  const [newComment, setNewComment] = useState("");
+
+  const handleDialogOpen = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+  };
+
+  const handleCommentChange = (event) => {
+    setNewComment(event.target.value);
+  };
+
+  const addComment = () => {
+    setComments((initialComments) => [...prevComments, newComment]);
+    setNewComment("");
+  };
+
+  return (
+    <div>
+      <img
+        src={imageSrc}
+        alt={altText}
+        onClick={handleDialogOpen}
+        style={{ cursor: "pointer" }}
+      />
+
+      <Dialog open={isDialogOpen} onClose={handleDialogClose}>
+        <DialogContent>
+          <img src={imageSrc} alt={altText} style={{ maxWidth: "100%" }} />
+
+          <div>
+            {comments.map((comment, index) => (
+              <div key={index}>{comment}</div>
+            ))}
+          </div>
+
+          <TextField
+            label="Add a Comment"
+            variant="outlined"
+            fullWidth
+            value={newComment}
+            onChange={handleCommentChange}
+          />
+
+          <Button variant="contained" color="primary" onClick={addComment}>
+            Add Comment
+          </Button>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
 
-export default Photoclick;
+export default ImageWithDialog;
