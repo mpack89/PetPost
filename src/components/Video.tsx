9dbc2ce43c57ae34e99c1ac1c1ee43078c8fab20
Michael Pack <packmichael@hotmail.com>
2023-12-19 21:12:49 -0330

    prop changes

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,15 +3,25 @@ import Carousel from "react-material-ui-carousel";
 import { Paper, Button } from "@mui/material";
 import data from "./photodata.json";
 
-export function Video(props) {
+export function Video(autoplay: false) {
   const images = data.photos;
 
   return (
-    <Carousel>
-      {images.map((image, i) => (
-        <Item key={i} image={image} />
-      ))}
-    </Carousel>
+    <div
+      style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+        overflowY: "auto",
+      }}
+    >
+      <Carousel autoPlay={false}>
+        {images.map((image, i) => (
+          <Item key={i} image={image} />
+        ))}
+      </Carousel>
+    </div>
   );
 }
 
@@ -19,14 +29,14 @@ export function Item({ image }) {
   return (
     <Paper sx={{ width: 400, height: 630, margin: 2, color: "black" }}>
       <img
-        style={{ width: "100%", height: "100%" }}
+        style={{ width: "100%", height: "80%" }}
         src={image.url}
         alt={image.name}
       />
-      <h2>{image.name}</h2>
+      <h2>{image.user}</h2>
       <p>{image.description}</p>
 
-      <Button className="CheckButton">Check it out!</Button>
+      <Button></Button>
     </Paper>
   );
 }
