f1242475792d85266075970972266aff13c18974
Michael Pack <packmichael@hotmail.com>
2024-02-20 18:15:50 -0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,19 @@ import { Video } from "./components/Video";
 import { Grid } from "@mui/material";
 import { useEffect } from "react";
 import data from "./components/Profilepage/data.json";
+import photodata from "./components/photodata.json";
 
 function App() {
   const localStorageData = localStorage.getItem("UPDATE_PROFILE");
   useEffect(() => {
     if (!localStorageData)
       localStorage.setItem("UPDATE_PROFILE", JSON.stringify(data.profile));
+  }, []);
+
+  const photoStorageData = localStorage.getItem("PHOTO_DATA");
+  useEffect(() => {
+    if (!photoStorageData)
+      localStorage.setItem("PHOTO_DATA", JSON.stringify(photodata.photos));
   }, []);
 
   return (
