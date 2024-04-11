7e7cc6b1874391bbab626dd64b2013cde5494772
Michael Pack <125394901+mpack89@users.noreply.github.com>
2024-04-10 22:17:54 -0230

    on enter added

--- src/components/AddCommentForm.tsx
+++ src/components/AddCommentForm.tsx
@@ -1,49 +1,48 @@
-import React, { useState } from "react";
-import { TextField, Button, Grid } from "@mui/material";
+import React, { useState } from 'react';
+import { TextField, Button, Grid } from '@mui/material';
 
 interface AddCommentFormProps {
   onAddComment: (commentText: string) => void;
 }
 
 const AddCommentForm: React.FC<AddCommentFormProps> = ({ onAddComment }) => {
-  const [commentText, setCommentText] = useState("");
+  const [commentText, setCommentText] = useState('');
 
-  const handleAddComment = () => {
-    if (commentText.trim() !== "") {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault(); 
+    if (commentText.trim() !== '') {
       onAddComment(commentText);
-      setCommentText("");
+      setCommentText(''); 
     }
   };
+
   return (
-    <Grid
-      container
-      spacing={2}
-      alignItems="center"
-      justifyContent="space-between"
-    >
-      <Grid item xs={9.7}>
-        <TextField
-          label="Add a comment..."
-          variant="outlined"
-          value={commentText}
-          onChange={(e) => setCommentText(e.target.value)}
-          fullWidth
-          sx={{
-            marginLeft: "20px",
-          }}
-        />
+    <form onSubmit={handleSubmit}>
+      <Grid container spacing={2} alignItems="center" justifyContent="space-between">
+        <Grid item xs={9.7}>
+          <TextField
+            label="Add a comment..."
+            variant="outlined"
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+            fullWidth
+            sx={{
+              marginLeft: '20px',
+            }}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <Button
+            type="submit" 
+            variant="contained"
+            color="primary"
+            size="small"
+          >
+            Post
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={2}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleAddComment}
-          size="small"
-        >
-          Post
-        </Button>
-      </Grid>
-    </Grid>
+    </form>
   );
 };
 
