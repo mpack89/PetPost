d510e413899bd6cd6d666bee39a96df5ef863037
Michael Pack <125394901+mpack89@users.noreply.github.com>
2024-04-17 21:55:58 -0230

    cleanup

--- src/store.ts
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import profileReducer from "./redux/userprofile/profileReducer";
-//import rootReducer from "./"
-const store = configureStore(profileReducer);
-
-export default store;
